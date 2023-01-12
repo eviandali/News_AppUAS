5ebb6d21c3023af96ddfd8aa831195e65bdd92ac
unknown <eviansdali@gmail.com>
2023-01-12 18:24:07 +0800

    first commit

---
+++ lib/Service_Api/Service_API_Sport.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:uas/model/article_model.dart';
+
+class ApiService {
+  final endPointUrl = "newsapi.org";
+  final client = http.Client();
+  Future<List<Article>> getArticle() async {
+    final queryParameters = {
+      'q': 'Olahraga',
+      'apiKey': '66666e29b9ae467c9de8b0ecd712e646'
+    };
+    final uri = Uri.https(endPointUrl, '/v2/everything', queryParameters);
+    final response = await client.get(uri);
+    Map<String, dynamic> json = jsonDecode(response.body);
+    List<dynamic> body = json['articles'];
+    List<Article> articles =
+        body.map((dynamic item) => Article.fromJson(item)).toList();
+    return articles;
+  }
+}
---
+++ lib/components/customListTile.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:uas/model/article_model.dart';
+import 'package:uas/pages/artilces_detail_page.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:animate_gradient/animate_gradient.dart';
+
+Widget customListTile(Article article, BuildContext context) {
+  return InkWell(
+    onTap: () {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => ArticlePage(
+                    article: article,
+                  )));
+    },
+    child: Container(
+      margin: EdgeInsets.all(12.0),
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+          color: Color.fromARGB(52, 0, 0, 0),
+          borderRadius: BorderRadius.circular(12.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 3.0,
+            )
+          ]),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          article.urlToImage != null
+              ? Container(
+                  height: 200.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(article.urlToImage),
+                        fit: BoxFit.cover),
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                )
+              : Container(
+                  height: 200.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            'https://source.unsplash.com/weekly?coding'),
+                        fit: BoxFit.cover),
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                ),
+          SizedBox(height: 8.0),
+          Container(
+            padding: EdgeInsets.all(6.0),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(118, 255, 255, 255),
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            child: Text(
+              article.source.name,
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            article.title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16.0,
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
